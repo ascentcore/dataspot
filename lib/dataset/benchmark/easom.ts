b7db99556ec7f6df072b1fbf0c905d99a4abc58f
Sara Popa <sara.c.popa.14@gmail.com>
2020-11-03 16:09:40 +0200

    Sphere and Easom Functions

---
+++ lib/dataset/benchmark/easom.ts
@@ -0,0 +1,9 @@
+export default function calculate(x1: number, x2: number): number {
+    let value = 0
+    value = -Math.cos(x1) * Math.cos(x2) * Math.exp(-((x1 - Math.PI) ** 2) - ((x2 - Math.PI) ** 2))
+
+    return value
+}
+
+export const DOMAIN = [-100, 100]
+export const GLOBAL_M = [Math.PI]
