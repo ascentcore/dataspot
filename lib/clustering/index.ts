8a56680a54901198e98bf8a29acbda8090afc5ca
Cornel Stefanache <cornel.stefanache@gmail.com>
2020-11-24 08:24:31 +0200

    added jekyll to project for github pages

---
+++ lib/clustering/index.ts
@@ -0,0 +1,15 @@
+export * from './kMeans'
+export * from './meanShift'
+export * from './dbScan'
+export * from './agglomerativeClustering'
+export * from './clustering'
+export * from './centroidClustering'
+
+/**
+ * This is the base class for clustering
+ *
+ * @include: frontend/clustering
+ *
+ * another documentation line
+ */
+export function documentation() {}
