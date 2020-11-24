5ac9229063619dd1ce1fe76ab6bea5e979d575db
Corina Adam <adam97corina@gmail.com>
2020-11-24 14:39:21 +0200

    add pso algorithm

---
+++ lib/pso/_pso.spec.ts
@@ -0,0 +1,17 @@
+import calculate, { DOMAIN, GLOBAL_M } from '../dataset/benchmark/ackley'
+import PSO, { PSOConfig, FitnessFunction } from './pso'
+
+const pso = new PSO(<PSOConfig>{ populationSize: 3 })
+
+describe('PSO', () => {
+    it('performs pso on ackley', async (done) => {
+        const particles = await pso.fit(
+            Object.assign(new FitnessFunction(), {
+                function: calculate,
+                dimensions: [{ min: DOMAIN[0], max: DOMAIN[1] }, { min: DOMAIN[0], max: DOMAIN[1] }]
+            })
+        )
+        console.log(particles, GLOBAL_M)
+        done()
+    })
+})
---
+++ lib/pso/pso.ts
@@ -0,0 +1,169 @@
+/* eslint-disable operator-assignment, no-param-reassign */
+import EvolutionaryAlgorithm, { EvolutionaryConfig } from '../common/evolutionaryAlgorithm'
+import Random from '../math/random'
+
+export class PSOConfig extends EvolutionaryConfig {
+    public inertiaWeight: number = 0.75
+
+    public cognitiveWeight: number = 0.1
+
+    public socialWeight: number = 0.3
+
+    public populationSize: number = 100
+}
+
+export class FitnessFunction {
+    public function!: Function
+
+    public dimensions!: { min: number; max: number }[]
+}
+
+export class Individual {
+    public id: number = 0
+
+    public position: number[] = []
+
+    public fitness = Infinity
+
+    public bestPosition: number[] = []
+
+    public bestFitness = Infinity
+
+    public velocity: number[] | null = null
+
+    computeFitness(fitnessFunction: FitnessFunction) {
+        this.fitness = fitnessFunction.function(...this.position)
+        if (this.fitness < this.bestFitness) {
+            for (let i = 0; i < this.position.length; i++) {
+                this.bestPosition[i] = this.position[i]
+            }
+            this.bestFitness = this.fitness
+        }
+    }
+}
+
+export default class PSO extends EvolutionaryAlgorithm<PSOConfig> {
+    protected individuals!: Individual[]
+
+    protected bestPosition!: number[]
+
+    protected initialized = false
+
+    protected fitnessFunction!: FitnessFunction
+
+    constructor(config?: PSOConfig | undefined) {
+        super(Object.assign(new PSOConfig(), config))
+    }
+
+    canStop(): boolean {
+        return false
+    }
+
+    private movePosition(particle: Individual) {
+        const { dimensions } = this.fitnessFunction
+        const { inertiaWeight, cognitiveWeight, socialWeight } = this.config
+
+        if (!particle.velocity) {
+            particle.velocity = []
+            for (let i = 0; i < dimensions.length; i++) {
+                const d = dimensions[i].max - dimensions[i].min
+                particle.velocity.push(Random.random(-d, d))
+            }
+        }
+        for (let i = 0; i < dimensions.length; i++) {
+            const vMomentum = inertiaWeight * particle.velocity[i]
+
+            const d1 = particle.bestPosition[i] - particle.position[i]
+            const vCognitive = cognitiveWeight * Random.random(0, 1) * d1
+
+            const d2 = this.bestPosition[i] - particle.position[i]
+            const vSocial = socialWeight * Random.random(0, 1) * d2
+
+            particle.velocity[i] = vMomentum + vCognitive + vSocial
+            particle.position[i] = particle.position[i] + particle.velocity[i]
+
+            if (particle.position[i] > dimensions[i].max) {
+                particle.position[i] = dimensions[i].max
+            }
+            if (particle.position[i] < dimensions[i].min) {
+                particle.position[i] = dimensions[i].min
+            }
+        }
+    }
+
+    sortPopulation() {
+        this.individuals.sort((i1, i2) => i1.fitness - i2.fitness)
+    }
+
+    step() {
+        for (let i = 0; i < this.individuals.length; i++) {
+            this.movePosition(this.individuals[i])
+        }
+        for (let i = 0; i < this.individuals.length; i++) {
+            this.individuals[i].computeFitness(this.fitnessFunction)
+        }
+        this.sortPopulation()
+        this.updateGlobalBest()
+    }
+
+    private updateGlobalBest(): void {
+        if (!this.individuals.length) {
+            throw new Error(
+                'Algorithm was not initialized. Call preparePopulation or load pretrained configuration using loadState'
+            )
+        }
+
+        this.bestPosition = [...this.individuals[0].bestPosition, this.individuals[0].bestFitness]
+
+        for (let i = 1; i < this.individuals.length; i++) {
+            if (this.individuals[i].bestFitness < this.bestPosition.slice(-1)[0]) {
+                this.bestPosition = [...this.individuals[i].bestPosition, this.individuals[i].bestFitness]
+            }
+        }
+    }
+
+    preparePopulation(): void {
+        const { populationSize } = this.config
+        this.iteration = 0
+        this.individuals = []
+        for (let i = 0; i < populationSize; i++) {
+            const p = new Individual()
+            for (let j = 0; j < this.fitnessFunction.dimensions.length; j++) {
+                const randomNumber = Random.random(
+                    this.fitnessFunction.dimensions[j].min,
+                    this.fitnessFunction.dimensions[j].max
+                )
+                p.position.push(randomNumber)
+                p.bestPosition.push(randomNumber)
+            }
+            this.individuals.push(p)
+        }
+        this.updateGlobalBest()
+    }
+
+    *fitAsync(fitessFunction: FitnessFunction): Generator {
+        this.fitnessFunction = fitessFunction
+        this.preparePopulation()
+        while (!this.shouldStop() && !this.canStop()) {
+            this.step()
+            yield this.individuals.map((individual: Individual) => individual.position)
+            this.iteration++
+        }
+        this.onEndFit()
+        return this.individuals.map((individual: Individual) => individual.position)
+    }
+
+    fit(fitessFunction: FitnessFunction): number[][] {
+        this.fitnessFunction = fitessFunction
+        this.preparePopulation()
+        while (!this.shouldStop() && !this.canStop()) {
+            this.step()
+            this.iteration++
+        }
+        this.onEndFit()
+
+        return this.individuals.map((individual: Individual) => individual.position)
+    }
+
+    onEndFit(): void {}
+}
