95e0f796095eefb970903c783c425883305361be
Cornel Stefanache <cornel.stefanache@gmail.com>
2020-12-07 11:03:48 +0200

    documentation: clustering base

--- lib/clustering/clusteringBase.ts
+++ lib/clustering/clusteringBase.ts
@@ -1,12 +1,28 @@
-import EvolutionaryAlgorithm, { EvolutionaryConfig } from '../common/evolutionaryAlgorithm'
+import EvolutionaryAlgorithm, { EvolutionaryConfig } from '@/common/evolutionaryAlgorithm'
 
+/**
+ * Base class for any clustering algorithms.
+ */
 export default abstract class ClusteringAlgorithm<T extends EvolutionaryConfig> extends EvolutionaryAlgorithm<T> {
+    /**
+     * Base data for fitted algorithm
+     */
     protected fitData!: number[][]
 
+    /**
+     * Stored labels after the initial fit
+     */
     protected labels!: number[]
 
+    /**
+     * Initialization state
+     */
     protected initialized = false
 
+    /**
+     * Prepares dataset and local state for fitting
+     * @param data data to be fitted
+     */
     protected prepareDataset(data: number[][]): void {
         if (!data || data.length === 0) {
             throw new Error('Unable to perform fitting without data')
@@ -16,6 +32,12 @@ export default abstract class ClusteringAlgorithm<T extends EvolutionaryConfig> 
         this.labels = new Array(data.length).fill(-1)
     }
 
+    /**
+     * Asynchronous data fitting
+     *
+     * @param data data to be fitted
+     * @returns generator function
+     */
     *fitAsync(data: number[][]): Generator {
         this.prepareDataset(data)
         while (!this.shouldStop() && !this.canStop()) {
@@ -27,6 +49,12 @@ export default abstract class ClusteringAlgorithm<T extends EvolutionaryConfig> 
         return this.labels
     }
 
+    /**
+     * Synchronous data fitting
+     *
+     * @param data data to be fitted
+     * @returns fitted labels
+     */
     fit(data: number[][]): number[] {
         this.prepareDataset(data)
         while (!this.shouldStop() && !this.canStop()) {
@@ -38,9 +66,19 @@ export default abstract class ClusteringAlgorithm<T extends EvolutionaryConfig> 
         return this.labels
     }
 
+    /**
+     * Function executed after clustering is completed
+     */
     onEndFit(): void {}
 
+    /**
+     * Flag to check if the algorithm converged
+     */
     abstract canStop(): boolean
 
+    /**
+     * Cluster new data
+     * @param data new data to be clustered after fit
+     */
     abstract predict(data: number[][]): number[]
 }
